73e36fc89867a4643660c01407526466f2b1f33c
ivanov <maxim2003.maks@mail.ru>
2024-05-17 23:15:46 +0300

    #3 Создан DataController для загрузки данных о маршрутах автобусов. Успешная загрузка тестовых записей в каждую таблицу бд

---
+++ backend/src/main/java/ru/busoptimizer/backend/controller/DataController.java
@@ -0,0 +1,69 @@
+package ru.busoptimizer.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.busoptimizer.backend.dto.BusesDto;
+import ru.busoptimizer.backend.dto.BusesPointsDto;
+import ru.busoptimizer.backend.dto.PointsDto;
+import ru.busoptimizer.backend.dto.StopsDto;
+import ru.busoptimizer.backend.entity.Buses;
+import ru.busoptimizer.backend.entity.BusesPoints;
+import ru.busoptimizer.backend.entity.Points;
+import ru.busoptimizer.backend.entity.Stops;
+import ru.busoptimizer.backend.mapper.BusesMapperImpl;
+import ru.busoptimizer.backend.mapper.BusesPointsMapperImpl;
+import ru.busoptimizer.backend.mapper.PointsMapperImpl;
+import ru.busoptimizer.backend.mapper.StopsMapperImpl;
+import ru.busoptimizer.backend.service.BusesPointsService;
+import ru.busoptimizer.backend.service.BusesService;
+import ru.busoptimizer.backend.service.PointsService;
+import ru.busoptimizer.backend.service.StopsService;
+
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/")
+public class DataController {
+    private final BusesService busesService;
+    private final BusesMapperImpl busesMapper;
+    private final StopsService stopsService;
+    private final StopsMapperImpl stopsMapper;
+    private final PointsService pointsService;
+    private final PointsMapperImpl pointsMapper;
+    private final BusesPointsService busesPointsService;
+    private final BusesPointsMapperImpl busesPointsMapper;
+
+    @PostMapping("/insert")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public HttpStatus addBusTest(@RequestBody Map<String, Object> json) {
+        String busName = (String) json.get("name");                                        //Название Автобуса
+
+        Buses bus = busesService.saveBus(busesMapper.toEntity(new BusesDto(busName)));
+
+        List<Object> bothDirections = (List<Object>) json.get("features");
+        Map<String, Object> forwardDirection = (Map<String, Object>) bothDirections.get(0);
+        List<Map<String, Object>> allPoints = (List<Map<String, Object>>) forwardDirection.get("features");
+
+        Map<String, Object> firstPoint = allPoints.get(0);
+        String stopName = (String) firstPoint.get("name");                              //Название Остановки
+        List<Double> pointCoordinates = (List<Double>) firstPoint.get("coordinates");   //Координаты точки остановки
+
+        Stops stop = stopsService.saveStop(stopsMapper.toEntity(new StopsDto(stopName)));
+
+        Points point = pointsService.savePoint(pointsMapper.toEntity(new PointsDto(
+                pointCoordinates.get(1),
+                pointCoordinates.get(0),
+                stop.getId())));
+
+        BusesPoints busPoint = busesPointsService.saveBusPoint(busesPointsMapper.toEntity(new BusesPointsDto(
+                bus.getId(),
+                point.getId(),
+                false)));
+
+        return HttpStatus.OK;
+    }
+
+}
